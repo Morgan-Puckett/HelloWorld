77b134bd97b4a55d7abc660c33681c75c4d0ceb0
Morgan Puckett <morganpuckett02@gmail.com>
2022-01-11 08:54:33 -0700

    Add project files.

---
+++ HelloWorld/HelloWorld.vb
@@ -0,0 +1,16 @@
+﻿'Morgan Puckett
+'RCET0265 S2022
+'HW1, HelloWorld
+Module HelloWorld
+
+    Sub Main()
+        Console.WriteLine("Hello World!")
+        Console.ReadLine()
+        Console.Beep()
+        Console.WriteLine("It was good meeting you too")
+        Console.Beep()
+        Console.Beep()
+        Console.Beep()
+    End Sub
+
+End Module
